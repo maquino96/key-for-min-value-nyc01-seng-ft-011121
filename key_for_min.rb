29739777e1fad623eba26e801827df2a036eb151
Matthew Aquino <matthewaquino96@gmail.com>
2021-01-04 01:15:44 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,15 +2,15 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  min = 0 
+  min = '' 
   if name_hash.count == 0 
     return nil  
   else
     name_hash.each do |key, value|
       if value < min 
-        min = value
+        min = key
       end
     end
   end
-  min
+  key
 end
