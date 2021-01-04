04e1ad3932cd6f3493f089fca795ac3f8388625f
Matthew Aquino <matthewaquino96@gmail.com>
2021-01-04 01:28:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  min = name_hash[0] 
+  min = 1000
   result = nil
   if name_hash.count == 0 
     return nil  
