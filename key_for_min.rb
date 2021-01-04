28c41382d55cf46e0ba07198e0680eeca91d1461
Matthew Aquino <matthewaquino96@gmail.com>
2021-01-04 01:33:07 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  min = 1000000
+  min = 1000000 #this code could be abstracted more so that a number isn't hard coded here or insert an astronomically large number to encompass all inputs. 
   result = nil
   if name_hash.count == 0 
     return nil  
